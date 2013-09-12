733514cdc7c85bef2ecad9d21f44a91a3e0e00d5
VarchukVladimir <fildes@gmail.com>
2013-09-12 19:26:24 +0300

    Added function of generation JSON file from environmet variables

--- src/filesender.c
+++ src/filesender.c
@@ -9,8 +9,75 @@
 #include <string.h>
 #include <stdlib.h>
 #include "zvmfileutil.h"
+#include <unistd.h>
 
 #undef TEST
+
+extern char **environ;
+
+char *myrealloc (char * buff, size_t *buffsize)
+{
+	char *newbuff;
+	char *oldbuff;
+	size_t newbuffsize = *buffsize * 2;
+	newbuff = (char *) malloc (sizeof (char) * newbuffsize);
+	if (newbuff)
+	{
+		memcpy (newbuff, buff, *buffsize);
+		free (buff);
+		buff = newbuff;
+		*buffsize = newbuffsize;
+	}
+	return buff;
+}
+
+char * generateJson ()
+{
+	size_t jsonmaxsize = 1024;
+	size_t jsonsize = 0;
+	char *json = (char *) malloc (sizeof (char) * jsonmaxsize);
+
+	sprintf (json, "{\n");
+	jsonsize = strlen (json) - 1;
+
+	int i;
+	for ( i = 0; environ[i] != NULL; i++)
+	{
+		int envsize = strlen (environ[i]);
+		//printf ("%s\n", environ[i]);
+		char *pKey = (char *) malloc (envsize * sizeof (char));
+		char *pVal = (char *) malloc (envsize * sizeof (char));
+		char *pEq;
+		int eqPos = 0;//
+
+		if ((pEq = strstr(environ[i], "=")) != NULL)
+			eqPos = pEq - environ[i];
+
+		if (eqPos == 0)
+			continue;
+		//copy key && value
+		strncpy ( pKey, environ[i], eqPos );
+		strncpy ( pVal, environ[i] + eqPos + 1, envsize - eqPos );
+
+		pKey [eqPos] = '\0';
+
+		size_t addsize = strlen (pKey) +  strlen (pVal) + 8; //8 symbols plus -------  1 '\t' + 1 '=' + 4 '"' + 1 ',' + 1 '\n'
+
+		if ((jsonsize + addsize) > jsonmaxsize )
+			json = myrealloc (json, &jsonmaxsize);
+
+		sprintf (json + jsonsize + 1, "\t\"%s\":\"%s\",\n",pKey, pVal);
+		jsonsize += addsize;
+	}
+
+	if (jsonsize + 2 > jsonmaxsize)
+		json = myrealloc (json, &jsonmaxsize);
+	sprintf (json + jsonsize - 1, "\n}");
+	jsonsize += 2;
+	return json;
+}
+
+
 
 int main (int argc, char *argv[])
 {
@@ -19,6 +86,8 @@ int main (int argc, char *argv[])
 
 	char *serversoft = getenv ("SERVER_SOFTWARE");
 	char *filename = NULL;
+
+	char *json = NULL;
 
 	printf ("serversoft=%s\n", serversoft);
 
@@ -41,6 +110,10 @@ int main (int argc, char *argv[])
 		filename = getenv("fname");
 	}
 
+	json = generateJson();
+
+	printf ("%s", json);
+
 	char ext[strlen (filename)];
 	getext(filename, ext);
 	if (strncmp (ext, "txt", 3) == 0 || strncmp (ext, "odt", 3) == 0 || strncmp (ext, "docx", 4) == 0)
@@ -52,5 +125,7 @@ int main (int argc, char *argv[])
 	struct filemap fmap = getfilefromchannel ("/home/volodymyr/data_test/txt", "/home/volodymyr/data_test"); ///
 	printf ("tmp name - %s; real file name - %s; size %d\n", fmap.tempfilename, fmap.realfilename, (int) fmap.realfilesize);
 #endif
+
+
 	return 0;
 }
