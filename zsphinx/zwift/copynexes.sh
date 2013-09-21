f0ccd826ce20674d0169794481cdb28017817018
VarchukVladimir <fildes@gmail.com>
2013-09-21 21:41:42 +0300

    index, config and json template update

--- zsphinx/zwift/copynexes.sh
+++ zsphinx/zwift/copynexes.sh
@@ -8,6 +8,7 @@ cp ../../src/xmlpipecreator 		search/sys/xmlpipecreator.nexe
 cp ../../src/indexer 			search/sys/indexer.nexe
 cp ../../src/filesender 		search/sys/filesender.nexe
 cp ../../src/search 			search/sys/search.nexe
+cp ../../src/other 			search/sys/other.nexe
 cp ../../zxpdf-3.03/xpdf/pdftotext 	search/sys/pdf.nexe
 cp ../../docxextract/docxtotext 	search/sys/txt.nexe
 cp ../../antiword-0.37/antiword 	search/sys/doc.nexe
