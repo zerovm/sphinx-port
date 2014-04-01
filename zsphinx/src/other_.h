9e2d23406f22e0786245d1f490a64bee93f218b9
VarchukVladimir <fildes@gmail.com>
2014-04-01 16:11:06 +0300

    other

---
+++ zsphinx/src/other_.h
@@ -0,0 +1,13 @@
+/*
+ * other_.h
+ *
+ *  Created on: Dec 17, 2013
+ *      Author: Volodymyr Varchuk
+ */
+
+#ifndef OTHER__H_
+#define OTHER__H_
+
+int other_main (int, char **);
+
+#endif /* OTHER__H_ */
